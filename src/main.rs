e6546147ed969440bc001cb8c00e5b6fdbf33380
akazukin5151 <tsuiyikching@protonmail.com>
2022-08-02 00:31:22 +0100

    Move generate_plot to plot.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod kde;
-use kde::*;
-use plotters::coord::Shift;
+mod plot;
 
-use plotters::prelude::*;
+use plot::*;
 use rand::distributions::Uniform;
 use rand::prelude::SliceRandom;
 use rand::Rng;
@@ -125,96 +124,5 @@ fn generate_data() -> (usize, Vec<(String, Vec<i32>)>, Vec<(String, Vec<f64>)>)
         }
     }
     (n_stations, station_stairs, train_passengers)
-}
-
-fn generate_plot(
-    (n_stations, station_stairs, train_passengers): (
-        usize,
-        Vec<(String, Vec<i32>)>,
-        Vec<(String, Vec<f64>)>,
-    ),
-) -> Result<
-    DrawingArea<BitMapBackend<'static>, Shift>,
-    Box<dyn std::error::Error>,
-> {
-    let root =
-        BitMapBackend::new("out/out.png", (1024, 768)).into_drawing_area();
-    root.fill(&WHITE)?;
-
-    let black_stroke = ShapeStyle {
-        color: RGBAColor(0, 0, 0, 1.0),
-        filled: true,
-        stroke_width: 1,
-    };
-
-    let lighter_stroke = ShapeStyle {
-        color: GREEN.mix(1.0),
-        filled: true,
-        stroke_width: 1,
-    };
-
-    let roots = root.split_evenly((n_stations, 1));
-    for (i, root) in roots.iter().enumerate() {
-        let mut chart = ChartBuilder::on(&root)
-            .margin_left(10)
-            .margin_right(30)
-            .margin_top(10)
-            .margin_bottom(10)
-            .x_label_area_size(40_i32)
-            .y_label_area_size(80_i32)
-            .build_cartesian_2d(0.0..100.0, 0.0..0.1)?;
-
-        let mut mesh = chart.configure_mesh();
-        let mesh = mesh.y_desc(&station_stairs[i].0).light_line_style(&WHITE);
-        if i == n_stations - 1 {
-            mesh.x_desc("xpos").draw()?;
-        } else {
-            mesh.draw()?;
-        }
-
-        let res: Vec<_> = (0..=100)
-            .map(|num| {
-                (
-                    num as f64,
-                    kernel_density_estimator(
-                        &train_passengers[i].1,
-                        scotts(train_passengers[i].1.len() as f64) * 12.0,
-                        num as f64,
-                    ),
-                )
-            })
-            .collect();
-
-        chart.draw_series(LineSeries::new(res, BLUE.filled()))?;
-
-        let drawing_area = chart.plotting_area();
-
-        let mapped = drawing_area.map_coordinate(&(0.0, 0.0));
-        let modifier = 250 * i as i32;
-        let p: PathElement<(i32, i32)> = PathElement::new(
-            [(mapped.0, 0), (mapped.0, mapped.1 - modifier)],
-            black_stroke,
-        );
-        root.draw(&p)?;
-
-        let mapped = drawing_area.map_coordinate(&(100.0, 0.0));
-        let p: PathElement<(i32, i32)> = PathElement::new(
-            [(mapped.0, 0), (mapped.0, mapped.1 - modifier)],
-            black_stroke,
-        );
-        root.draw(&p)?;
-
-        let (_, stair_locations) = &station_stairs[i];
-        for stair_location in stair_locations {
-            let mapped =
-                drawing_area.map_coordinate(&(*stair_location as f64, 0.0));
-            let p: PathElement<(i32, i32)> = PathElement::new(
-                [(mapped.0, 0), (mapped.0, mapped.1 - modifier)],
-                lighter_stroke,
-            );
-            root.draw(&p)?;
-        }
-    }
-    Ok(root)
 }
 
