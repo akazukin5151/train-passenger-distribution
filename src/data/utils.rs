3130bdd11db038340c02148e50d9607730f17bec
akazukin5151 <tsuiyikching@protonmail.com>
2022-08-05 16:16:47 +0100

    Use folded normal distribution for mean=zero and beta otherwise
    This allows us to eliminate the slight adjustment of values at the bounds
    because the beta distribution does not accept 0 and 1

--- src/data/utils.rs
+++ src/data/utils.rs
@@ -1,5 +1,10 @@
+use rand::distributions::Uniform;
+use rand::Rng;
 use rand_distr::Beta;
 use rand_distr::BetaError;
+use rand_distr::Distribution;
+use std::f64::consts::E;
+use std::f64::consts::PI;
 
 /// Reformulation of the beta distribution in terms of mean and standard deviation
 /// Originally it accepts two shapes, alpha and beta
@@ -10,16 +15,40 @@ pub fn beta(mean: f64, stdev: f64) -> Result<Beta<f64>, BetaError> {
     Beta::new(alpha, beta)
 }
 
+pub fn folded_normal(mean: f64, stdev: f64, x: f64) -> f64 {
+    let common = 1.0 / (stdev * (2.0 * PI).powf(0.5));
+    let positive = E.powf(-0.5 * ((x + mean) / stdev).powi(2));
+    let negative = E.powf(-0.5 * ((x - mean) / stdev).powi(2));
+    common * (positive + negative)
+}
+
+pub struct FoldedNormal {
+    mean: f64,
+    stdev: f64,
+}
+
+impl FoldedNormal {
+    pub fn new(mean: f64, stdev: f64) -> FoldedNormal {
+        FoldedNormal { mean, stdev }
+    }
+}
+
+impl Distribution<f64> for FoldedNormal {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> f64 {
+        folded_normal(self.mean, self.stdev, rng.sample(Uniform::new(0.0, 1.0)))
+    }
+}
+
 pub fn standardize_between(max: f64, min: f64, xs: Vec<f64>) -> Vec<f64> {
     xs.iter().map(|x| (x - min) / (max - min) * 100.0).collect()
 }
 
 pub fn clamp(x: f64) -> f64 {
-    if x > 0.0 && x < 100.0 {
+    if x >= 0.0 && x <= 100.0 {
         x
     } else if x > 100.0 {
-        99.9
+        100.0
     } else {
-        0.01
+        0.0
     }
 }
