a6f017a19bcf07c48527afb6be862d0c9f5a3cab
akazukin5151 <tsuiyikching@protonmail.com>
2022-08-18 11:27:40 +0100

    Plot combination of boarding types for each stair and all boarders

--- src/plot/steps.rs
+++ src/plot/steps.rs
@@ -1,7 +1,6 @@
 use crate::kde::make_kde;
 use crate::plot::colors::*;
 use crate::plot::utils::*;
-use crate::sum_boarding_types;
 use crate::types::*;
 use crate::Accumulator;
 use crate::COLORS;
@@ -23,8 +22,8 @@ fn plot_initial<T>(
 
     // plot
     let root = &roots[0];
-    root.titled("Initial distribution from Tokyo", ("sans-serif", 30))?;
-    let mut chart = chart_with_mesh!(root, 0.0..0.06_f64);
+    root.titled("Initial distribution from Kanda", ("sans-serif", 30))?;
+    let mut chart = chart_with_mesh!(root, 0.0..0.1_f64);
 
     chart.draw_series(LineSeries::new(kde, BLUE.stroke_width(2)))?;
 
@@ -48,7 +47,7 @@ fn plot_alighting(
 
     // plot
     let root = &roots[1];
-    root.titled("Passengers alighting at Kanda", ("sans-serif", 30))?;
+    root.titled("Passengers alighting at Ochanomizu", ("sans-serif", 30))?;
     let mut chart = chart_with_mesh!(root, 0.0..1.0_f64);
 
     plot_points(&mut chart, &mut alight_xs, alight_ys, RED, "Alighting")?;
@@ -74,10 +73,10 @@ fn plot_boarding(
         roots.iter().skip(2).zip(boarding_data).enumerate()
     {
         root.titled(
-            &format!("Passengers boarding at Kanda stair #{}", i + 1),
+            &format!("Passengers boarding at Ochanomizu stair #{}", i + 1),
             ("sans-serif", 30),
         )?;
-        let mut chart = chart_with_mesh!(root, 0.0..0.15_f64);
+        let mut chart = chart_with_mesh!(root, 0.0..0.25_f64);
 
         let labels = ["beta far", "beta close", "uniform"];
         for ((xs, label), color) in
@@ -102,7 +101,38 @@ fn plot_boarding(
     Ok(())
 }
 
+fn plot_all_boarding(
+    roots: &[DrawingArea<BitMapBackend, Shift>],
+    boarding_data: &BoardingData,
+    multiplier: f64,
+) -> Result<(), Box<dyn std::error::Error>> {
+    for (i, (root, (stair, far, close, uni))) in
+        roots.iter().skip(2).zip(boarding_data).enumerate()
+    {
+        let mut acc: Vec<f64> = vec![];
+        acc.extend(far);
+        acc.extend(close);
+        acc.extend(uni);
+
+        root.titled(
+            &format!("All passengers boarding at Ochanomizu stair #{}", i + 1),
+            ("sans-serif", 30),
+        )?;
+        let mut chart = chart_with_mesh!(root, 0.0..0.25_f64);
+
+        let kde = make_kde(multiplier, &acc);
+        chart.draw_series(LineSeries::new(kde, BLUE.stroke_width(2)))?;
+
+        let modifier = (209 * i + 434) as i32;
+        plot_platform_bounds(&chart, root, modifier, 35)?;
+
+        plot_stairs(root, &chart, *stair, modifier, 35)?;
+    }
+    Ok(())
+}
+
 fn plot_combined<T>(
+    title: &str,
     xs: &[f64],
     roots: &[DrawingArea<BitMapBackend, Shift>],
     multiplier: f64,
@@ -113,9 +143,9 @@ fn plot_combined<T>(
 
     // plot
     let root = &roots[6];
-    root.titled("Net distribution after Kanda", ("sans-serif", 30))?;
+    root.titled(title, ("sans-serif", 30))?;
 
-    let mut chart = chart_with_mesh!(root, 0.0..0.06_f64);
+    let mut chart = chart_with_mesh!(root, 0.0..0.1_f64);
 
     chart.draw_series(LineSeries::new(kde, BLUE.stroke_width(2)))?;
 
@@ -137,39 +167,42 @@ pub fn plot_step_by_step(
     Box<dyn std::error::Error>,
 > {
     // data
-    let tokyo_step = &all_steps[0];
-    let tokyo_boarding_data = &tokyo_step.0;
-    let tokyo_xs = sum_boarding_types(tokyo_boarding_data);
+    let initial_step = &all_steps[1];
+    let initial_boarding_data = &initial_step.0;
+    let initial_xs = &initial_step.3;
 
-    let kanda_step = &all_steps[1];
-    let kanda_boarding_data = &kanda_step.0;
-    let n_passengers_alighting = &kanda_step.1;
+    let this_step = &all_steps[2];
+    let this_boarding_data = &this_step.0;
+    let n_passengers_alighting = &this_step.1;
 
     // alighting
-    let alight_xs: Vec<_> = tokyo_xs
+    let alight_xs: Vec<_> = initial_xs
         .choose_multiple(
             &mut rand::thread_rng(),
             (*n_passengers_alighting).try_into().unwrap(),
         )
         .collect();
 
-    let remaining_xs = &kanda_step.2;
+    let remaining_xs = &this_step.2;
 
     // combined
-    let all_xs: &Vec<_> = &kanda_step.3;
+    let all_xs: &Vec<_> = &this_step.3;
 
     // plot
-    let root = BitMapBackend::new(filename, (1024, 1500)).into_drawing_area();
+    let root =
+        BitMapBackend::new(filename, (1024 * 2, 1500)).into_drawing_area();
     root.fill(&WHITE)?;
 
     // plus n_stairs for passengers boarding from kanda
     // plus one for passengers already in the train
     // plus one for passengers alighting in kanda
     // plus one for combination of all above
-    let n_stairs = kanda_boarding_data.len();
-    let roots = root.split_evenly((n_stairs + 3, 1));
+    let n_stairs = this_boarding_data.len();
+    let (left, right) = root.split_horizontally(1024);
+    let right_roots = right.split_evenly((n_stairs + 3, 1));
+    let roots = left.split_evenly((n_stairs + 3, 1));
 
-    plot_initial(&roots, &tokyo_xs, multiplier, tokyo_boarding_data)?;
+    plot_initial(&roots, initial_xs, multiplier, initial_boarding_data)?;
 
     plot_alighting(
         &roots,
@@ -177,9 +210,33 @@ pub fn plot_step_by_step(
         &mut remaining_xs.iter(),
     )?;
 
-    plot_boarding(&roots, kanda_boarding_data, multiplier)?;
+    plot_boarding(&roots, this_boarding_data, multiplier)?;
 
-    plot_combined(all_xs, &roots, multiplier, kanda_boarding_data)?;
+    plot_all_boarding(&right_roots, this_boarding_data, multiplier)?;
+
+    let mut bs = vec![];
+    for (_, far, close, uni) in this_boarding_data {
+        let mut acc: Vec<f64> = vec![];
+        acc.extend(far);
+        acc.extend(close);
+        acc.extend(uni);
+        bs.extend(acc);
+    }
+    plot_combined(
+        "All boarders at Ochanomizu",
+        &bs,
+        &right_roots,
+        multiplier,
+        this_boarding_data,
+    )?;
+
+    plot_combined(
+        "Net distribution after Ochanomizu",
+        all_xs,
+        &roots,
+        multiplier,
+        this_boarding_data,
+    )?;
 
     Ok(root.clone())
 }
